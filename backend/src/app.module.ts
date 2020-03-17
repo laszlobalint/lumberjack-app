deb2d5af40b2a379e75f2c5e1e9a6598cae818d9
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-17 23:10:48 +0100

    Configure typeorm.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './controllers/app.controller';
+import { Customer } from './entities/customer.entity';
+import { Product } from './entities/product.entity';
+import { Purchase } from './entities/purchase.entity';
+import { User } from './entities/user.entity';
+import { AppService } from './services/app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      synchronize: true,
+      entities: [User, Product, Purchase, Customer],
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
