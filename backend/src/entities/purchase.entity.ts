bc3558363dd3284546f731778664f72b00b99500
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-17 22:05:48 +0100

    Created entities.

--- backend/src/entities/purchase.entity.ts
+++ backend/src/entities/purchase.entity.ts
@@ -1,7 +1,40 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Customer } from './customer.entity';
+import { Product } from './product.entity';
+import { User } from './user.entity';
 
 @Entity()
 export class Purchase {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: string;
+
+  @ManyToOne(
+    type => User,
+    user => user.purchases,
+  )
+  user: User;
+
+  @Column({ type: 'double'})
+  amount: number;
+
+  @Column({ type: 'varchar' })
+  description: string;
+
+  @CreateDateColumn({ type: 'datetime' })
+  date: Date;
+
+  @Column({ type: 'boolean' })
+  completed: boolean;
+
+  @ManyToOne(
+    type => Product,
+    product => product.purchases,
+  )
+  product: Product;
+
+  @ManyToOne(
+    type => Customer,
+    customer => customer.purchases,
+  )
+  customer: Customer;
 }
