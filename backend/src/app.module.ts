5a12d2efcfce90e51aaaad1184356ad9cd808645
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-20 20:10:18 +0100

    Configure typeorm with .env.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
@@ -7,30 +7,11 @@ import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 import { PurchaseModule } from './purchase/purchase.module';
 import { UserModule } from './user/user.module';
-import { Customer } from './_entities/customer.entity';
-import { Product } from './_entities/product.entity';
-import { Purchase } from './_entities/purchase.entity';
-import { User } from './_entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'mariadb',
-          host: configService.get<string>('DATABASE_HOST'),
-          port: configService.get<number>('DATABASE_PORT'),
-          username: configService.get<string>('DATABASE_USERNAME'),
-          password: configService.get<string>('DATABASE_PASSWORD'),
-          database: configService.get<string>('DATABASE_NAME'),
-          entities: [User, Customer, Product, Purchase],
-          synchronize: true,
-        };
-      },
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRoot(),
     UserModule,
     CustomerModule,
     ProductModule,
