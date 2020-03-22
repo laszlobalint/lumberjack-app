b996f7416f9e9db765611db406ad0ea1ff4cd6d6
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-22 19:31:55 +0100

    Update migrations with role field; Rename user to admin,

--- backend/migrations/1584901775585-SeedUser.ts
+++ backend/migrations/1584901775585-SeedUser.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class SeedUser1584811411199 implements MigrationInterface {
+export class SeedUser1584901775585 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
       "INSERT INTO `user` (email, password, name, role) VALUES ('admin@lumberjack.com', '$2b$10$UcQ2iWrcXSh3C3g7EWnGEOD0GKubnpGGU8vf7A6Dv5ql5AXU8GvVu', 'Admin', 'admin')",
