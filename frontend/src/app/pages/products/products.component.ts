687f857433714a2d55b89dd0eeef501bc6518168
Balint Laszlo <balint.laszlo@t-systems.com>
2020-03-23 15:26:11 +0100

    Init products component3.

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'products',
+  selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
