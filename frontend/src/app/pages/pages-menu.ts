66f93bbd381a71ed757d1301b8239e792b93b7e4
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-25 12:09:10 +0100

    Change menu.

--- frontend/src/app/pages/pages-menu.ts
+++ frontend/src/app/pages/pages-menu.ts
@@ -8,36 +8,8 @@ export const MENU_ITEMS: NbMenuItem[] = [
     home: true,
   },
   {
-    title: 'FEATURES',
-    group: true,
-    icon: 'lock-outline',
-    children: [
-      {
-        title: 'Products',
-        link: '/pages/products',
-      },
-    ],
-  },
-  {
-    title: 'Auth',
-    icon: 'lock-outline',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
+    title: 'Products',
+    icon: 'shopping-bag-outline',
+    link: '/pages/products',
   },
 ];
