96773d93ac1e27313556b06dbaf6f7a4bca5bcfa
daniel.papilion <daniel.papilion@tekrama.com>
2020-03-26 10:53:29 +0100

    Remove group from menu.

--- frontend/src/app/pages/pages-menu.ts
+++ frontend/src/app/pages/pages-menu.ts
@@ -2,18 +2,13 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'PURCHASES',
-    icon: 'shopping-cart-outline',
-    group: true,
-  },
-  {
     title: 'New Purchase',
     icon: 'plus-square-outline',
     link: '/pages/purchases/create',
   },
   {
     title: 'All Purchases',
-    icon: 'list-outline',
+    icon: 'shopping-cart-outline',
     link: '/pages/purchases',
   },
   {
