fb484e7a2aba310a981c390577047c5aaba9d959
daniel.papilion <daniel.papilion@tekrama.com>
2020-04-02 15:19:55 +0200

    Heroku deploy configuration.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
 import { AppModule } from './app.module';
-
 import { DatabaseExceptionFilter } from './shared/exception-filters/database.exception-filter';
 import { HttpExceptionFilter } from './shared/exception-filters/http.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env['CORS_ORIGIN'] });
+  // app.enableCors({ origin: process.env['CORS_ORIGIN'] });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new DatabaseExceptionFilter(), new HttpExceptionFilter());
   const options = new DocumentBuilder()
@@ -20,6 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env['SERVER_PORT']);
+  await app.listen(process.env['SERVER_PORT'] || 3000);
 }
 bootstrap();
