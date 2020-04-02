930a1a769a311049a71d0020e968f77c096c5781
daniel.papilion <daniel.papilion@tekrama.com>
2020-04-02 19:24:02 +0200

    Rename env variable SERVER_PORT to PORT.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env['SERVER_PORT']);
+  await app.listen(process.env['PORT']);
 }
 bootstrap();
