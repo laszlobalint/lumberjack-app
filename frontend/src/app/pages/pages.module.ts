204bf044da61a4f4565253517fcf35f910da09f0
daniel.papilion <daniel.papilion@tekrama.com>
2020-04-10 19:50:24 +0200

    Add language and theme select to footer on si screen.

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { NbMenuModule, NbSelectModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 
 @NgModule({
-  imports: [PagesRoutingModule, ThemeModule, NbMenuModule, DashboardModule],
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    NbMenuModule,
+    DashboardModule,
+    NbSelectModule,
+  ],
   declarations: [PagesComponent],
 })
 export class PagesModule {}
