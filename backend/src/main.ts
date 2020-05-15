659a487159442c3f69a872026682fab91f9a449c
daniel.papilion <daniel.papilion@tekrama.com>
2020-05-15 16:41:48 +0200

    Do not serve swagger in production mode.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,8 @@ import { AppModule } from './app.module';
 import { DatabaseExceptionFilter } from './shared/exception-filters/database.exception-filter';
 import { HttpExceptionFilter } from './shared/exception-filters/http.exception-filter';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -20,14 +22,17 @@ async function bootstrap() {
   app.useGlobalFilters(new DatabaseExceptionFilter(), new HttpExceptionFilter());
   app.use(express.static(path.join(__dirname, 'public')));
 
-  const options = new DocumentBuilder()
-    .setTitle(process.env['PROJECT_NAME'])
-    .setDescription(process.env['PROJECT_DESCRIPTION'])
-    .setVersion(process.env['PROJECT_VERSION'])
-    .addTag(process.env['PROJECT_TAG'])
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  if (!isProduction) {
+    const options = new DocumentBuilder()
+      .setTitle(process.env['PROJECT_NAME'])
+      .setDescription(process.env['PROJECT_DESCRIPTION'])
+      .setVersion(process.env['PROJECT_VERSION'])
+      .addTag(process.env['PROJECT_TAG'])
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(process.env['PORT']);
 }
 
